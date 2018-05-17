5c47262cbfb032d741e6023f537365805837abc8
Tarek El Tamimi <st144918@stud.uni-stuttgart.de>
2018-05-17 17:53:50 +0200

    Initial commit

---
+++ src/main/java/de/studycard/studycard/StudycardApplication.java
@@ -0,0 +1,12 @@
+package de.studycard.studycard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudycardApplication.class, args);
+    }
+}
